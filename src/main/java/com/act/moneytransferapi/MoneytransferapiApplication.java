61d7bfcc49223ea3eac2d8b50bda177f3f084bfd
soreni <soreniroba@gmail.com>
2021-04-24 17:11:44 +0300

    controllers, services and repositories added

---
+++ src/main/java/com/act/moneytransferapi/MoneytransferapiApplication.java
@@ -0,0 +1,13 @@
+package com.act.moneytransferapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneytransferapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoneytransferapiApplication.class, args);
+    }
+
+}
